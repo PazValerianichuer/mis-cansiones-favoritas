730465bde22a7e37731b595878e349b35c0987c0
pazvaleria <106561758+PazValerianichuer@users.noreply.github.com>
2022-08-03 16:15:06 -0500

    cambio

--- js/pruevas.js
+++ js/pruevas.js
@@ -1,28 +1,28 @@
 function cambiar_porta_Voces_en_mi_interior() {
-    let titulo_cansion = document.querySelector("voses en mi interior-seleccionado h2");
-    titulo_cansion.innerHTML = "voses en mi interior";
-    let caja = document.querySelector("#cansion-seleccionada img");
+    let titulo_musica = document.querySelector("voses en mi interior-seleccionado h2");
+    titulo_musica.innerHTML = "voses en mi interior";
+    let caja = document.querySelector("#musica-seleccionada img");
     caja.src = "vistas/musica.png/porta.png";
-    let desc_cansion = document.querySelector("#cansion-seleccionada p");
+    let desc_musica = document.querySelector("#musica-seleccionada p");
     desc_juego.innerHTML = "voses en mi interior de porta con santaflow del album transtorno bipolar fue publicada en 2009 que relata la historia de un joven que sufre de bullyng escolar el cual con el paso del tiempo lo vuelve loco y crea un insendio en su aula de clases";
   }
 
   function cambiar_Dante_Titeres() {
-    let titulo_cansion = document.querySelector("#cansion-seleccionada h2");
-    titulo_cansion.innerHTML = "titeres";
-    let caja = document.querySelector("#cansion-seleccionada img");
+    let titulo_musica = document.querySelector("#musica-seleccionada h2");
+    titulo_musica.innerHTML = "titeres";
+    let caja = document.querySelector("#musica-seleccionada img");
     caja.src = "vistas/musica.png/Dante.png";
-    let desc_cansion = document.querySelector("#cansion-seleccionada p");
-    desc_cansion.innerHTML = "la cansion del artista Dante titeres fue lansada en 2020 sobre un tema de critica direscta a la sosiedad y politica actual";
+    let desc_musica = document.querySelector("#musica-seleccionada p");
+    desc_musica.innerHTML = "la cansion del artista Dante titeres fue lansada en 2020 sobre un tema de critica direscta a la sosiedad y politica actual";
   }
 
   function cambiar_interpuesto_Quien_te_cantara() {
-    let titulo_cansion = document.querySelector("#cansion-seleccionado h2");
-    titulo_cansion.innerHTML = "Quien te cantara";
-    let caja = document.querySelector("#cansion-seleccionado img");
+    let titulo_musica = document.querySelector("#cansion-seleccionado h2");
+    titulo_musica.innerHTML = "Quien te cantara";
+    let caja = document.querySelector("#musica-seleccionado img");
     caja.src = "vistas/musica.png/interpuesto.png";
-    let desc_cansion = document.querySelector("#cansion-seleccionado p");
-    desc_cansion.innerHTML = "la cancion de interpuesto quien te cantara es del album Fantasma Electrico su fecha de lanzamiento fue en 2014. ";
+    let desc_musica = document.querySelector("#musica-seleccionado p");
+    desc_musica.innerHTML = "la cancion de interpuesto quien te cantara es del album Fantasma Electrico su fecha de lanzamiento fue en 2014. ";
   }
   /*
   arreglo.length        Cantidad de elementos
@@ -31,13 +31,13 @@ function cambiar_porta_Voces_en_mi_interior() {
   arreglo.pop()         Eliminar el último elemento del arreglo
   arreglo.shift()       Eliminar el primer elementos del arreglo
 */
-let cansiones = ["Voses en mi interior", "Titeres", "Quien te cantara", "En boca de tantos", "las dos caras", "las cartas sobre la mesa", "palabras mudas", "hay siempre un sentimiento muerto en un corazon roto", "Fantasma enamorado", "Cinismo"];
-cansion[4] = "Battlefield";
-cansion.unshift("Doom");
-cansion.push("las cartas sobre la mesa");
+let musica = ["Voses en mi interior", "Titeres", "Quien te cantara", "En boca de tantos", "las dos caras", "las cartas sobre la mesa", "palabras mudas", "hay siempre un sentimiento muerto en un corazon roto", "Fantasma enamorado", "Cinismo"];
+musica[4] = "Battlefield";
+musica.unshift("Doom");
+musica.push("las cartas sobre la mesa");
 
-let contenedor = document.getElementById("cansiones-lista");
+let contenedor = document.getElementById("musica-lista");
 
-for(let i = 0; i < cansiones.length; i++) {
-    contenedor.innerHTML += "<button>" + cansiones[i] + "</button>";
+for(let i = 0; i < musica.length; i++) {
+    contenedor.innerHTML += "<button>" + musica[i] + "</button>";
   }
